8d4c979f7663b7fd76a101b1bf3083103b211769
vibo777 <54715728+vibo777@users.noreply.github.com>
2021-12-22 18:24:41 +0530

    models created
    user model & question model is done

---
+++ models/question.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+
+    title:{type:String,require:true},
+    description:{type:String, require:true},
+    category:{type:String,require:true},
+    doc:{type:String},
+    reward:{type:String,required:true,default:10},
+    user:{type:mongoose.Schema.Types.ObjectId,ref:"users"}
+
+})
+
+const questionModel = new mongoose.model('questions',questionSchema);
+module.exports = questionModel;
